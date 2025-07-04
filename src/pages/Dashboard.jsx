c0413c691a8a1829ff0805c2576d6953ef65213b
carrinson <folacarrinson@gmail.com>
2025-07-04 19:58:27 +0100

    are you working

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,14 @@ function Dashboard() {
     return (  
         <>
             <h1>this is a Dashboard</h1>
+            <div className="side-bar-con">
+            <ul className="side-bar">
+                <li>Profile</li>
+                <li>BMI</li>
+                <li>History</li>
+                <li>Settings</li>
+            </ul>
+            </div>
         </>
     );
 }
